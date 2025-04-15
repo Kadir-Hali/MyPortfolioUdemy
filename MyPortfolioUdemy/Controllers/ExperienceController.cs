522938c063ca2c9cd0b6650be73b197c5c76446d
Kadir-Hali <kadirhali07@gmail.com>
2025-04-15 17:52:21 +0300

    15.04.25-2

--- MyPortfolioUdemy/Controllers/ExperienceController.cs
+++ MyPortfolioUdemy/Controllers/ExperienceController.cs
@@ -1,12 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioUdemy.DAL.Context;
+using MyPortfolioUdemy.DAL.Entities;
 
 namespace MyPortfolioUdemy.Controllers
 {
     public class ExperienceController : Controller
     {
+        MyPortfolioContext context = new MyPortfolioContext();
         public IActionResult ExperienceList()
         {
+            var values = context.Experience.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateExperience()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateExperience(Experience experience)
+        {
+            context.Experience.Add(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+        public IActionResult DeleteExperience(int id)
+        {
+            var value = context.Experience.Find(id);   
+            context.Experience.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateExperience(int id)
+        {
+            var value = context.Experience.Find(id);
+            return View(value); 
+        }
+
+        [HttpPost]
+        public IActionResult UpdateExperience(Experience experience)
+        {
+            context.Experience.Update(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
         }
     }
 }
